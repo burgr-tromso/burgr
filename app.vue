18e2726b8029645995326917f4a79bf3232baf37
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2024-04-15 23:35:47 +0200

    Added analytics

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+
+
 </script>
+
+<script defer data-domain="burgr.gustavsen.app" src="https://plausible.io/js/script.js"></script>
+
 
 <template>
   <div class=" min-h-dvh overscroll-none bg-fixed bg-cover bg-center bg-[url('/assets/background-light.png')] dark:bg-[url('/assets/background-dark.png')]" >
