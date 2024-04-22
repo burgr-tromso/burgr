8cb824f7532eef40f5282f30e3d7c9070886afa3
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2024-04-22 11:42:28 +0200

    Updated textsize

--- components/card.vue
+++ components/card.vue
@@ -44,7 +44,7 @@ const props = defineProps({
           <MenuList :list="burgers" class="mb-12" />
 
           <div class="text-center space-y-0 " v-if="menyDeal.active">
-            <h2 class="font text-2xl">{{ menyDeal.label }}</h2>
+            <h2 class="font text-xl">{{ menyDeal.label }}</h2>
             <p>{{ menyDeal.price }},-</p>
             <p>Includes fries and a drink</p>
           </div>
