700ef48ca03fc7ecd4598b180bbabd9ca9b2e7f3
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2024-05-09 01:55:51 +0200

    Updated padding and some colors

--- components/card.vue
+++ components/card.vue
@@ -47,12 +47,12 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="divide-slate-600 rounded-md bg-slate-200 dark:bg-slate-700 bg-opacity-95 max-w-2xl mx-8 md:mx-auto shadow-xl">
+  <div class="rounded-md bg-slate-200 dark:bg-slate-700 bg-opacity-95 max-w-2xl mx-8 md:mx-auto shadow-xl">
     <TabGroup :selectedIndex="Number(route.query.item)" v-if="isMenu">
       <TabList class="grid grid-flow-col">
-        <Tab as="template" v-slot="{ selected }"v-for="tab in types" class="first:rounded-tl-md last:rounded-tr-md"><button class="h-12" :class=" { 'bg-slate-500 text-white shadow-2xl border-2 border-slate-600':selected, 'bg-slate-600 text-slate-100 hover:bg-slate-600': !selected } " @click="replaceQuery(tab.index)">{{ tab.label }}</button></Tab>
+        <Tab as="template" v-slot="{ selected }"v-for="tab in types" class="first:rounded-tl-md last:rounded-tr-md"><button class="h-12" :class=" { 'bg-slate-500 text-white shadow-2xl border-2 border-slate-600':selected, 'bg-slate-600 text-slate-100 hover:bg-slate-500': !selected } " @click="replaceQuery(tab.index)">{{ tab.label }}</button></Tab>
       </TabList>
-      <div class="border-x-slate-700 dark:border-x-slate-400 border-x-8 border-b-slate-700 dark:border-b-slate-400 border-b-8 rounded-b-md">
+      <div class="border-x-slate-700 dark:border-x-slate-600 border-x-8 border-b-slate-700 dark:border-b-slate-600 border-b-8 rounded-b-md">
         <TabPanels class="px-8">
           <TabPanel>
             <MenuList :list="burgers" class="mb-12" />
@@ -73,7 +73,7 @@ const props = defineProps({
           </TabPanel>
         </TabPanels>
         <p v-if="allergies"class=" md:px-24 px-8 pt-4 text-sm text-center md:text-base">{{ allergies }}</p>
-        <span v-if="isMenu" class="text-center">
+        <span v-if="isMenu" class="text-center p-16">
           <p class="mt-4"><Icon name="i-ph-leaf-bold" class="text-green-500" /> - Vegetarian</p>
           <p><Icon v-for="n in 3" name="i-ph-fire-bold" class="text-red-500" />  - Heatlevel!</p>
         </span>
