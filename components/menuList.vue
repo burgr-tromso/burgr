60831c135a7eb55ca13d80e2f8fb1a76d17d6aaa
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2024-05-09 11:03:23 +0200

    Update menuList.vue

--- components/menuList.vue
+++ components/menuList.vue
@@ -10,20 +10,24 @@ const props = defineProps({
 
 <template>
   <div class="pt-8">
-    <ul class="space-y-8" >
-        <li v-for="item in props.list" class="flex flex-col text-center">
+    <ul class="space-y-12" >
+        <li v-for="item in props.list" class=" grid grid-cols-2">
+          <div>
             <h3 class="font text-sm md:text-2xl" >{{ item.label }}<UIcon v-if="item.isVegetarian" class="text-green-500" name="i-ph-leaf-bold"/><UIcon v-if="item.spicelevel ?? 0 > 0" v-for="n in item.spicelevel ?? 0" name="i-ph-fire-bold" class="text-red-500" /></h3>
-          <h4 class="text-sm md:text-base" v-if="item.description" >{{ item.description }}</h4>
-          <p class="text-xs md:text-sm" v-if="item.allergies" >{{ item.allergies }}</p>
-          <!-- If only one item exits in sizes only show the price. Else show a list -->
-          <p v-if="item.sizes?.length == 1">
-            {{ item.sizes[0].price }},-
-          </p>
-          <ul v-else class="" >
-            <li class="font-semibold" v-for="size in item.sizes">
-              {{ size.size }}: {{ size.price }},-
-            </li>
-          </ul>
+            <h4 class="text-sm md:text-base" v-if="item.description" >{{ item.description }}</h4>
+            <p class="text-xs md:text-sm" v-if="item.allergies" >{{ item.allergies }}</p>
+          </div>
+          <div class="text-right">
+            <!-- If only one item exits in sizes only show the price. Else show a list -->
+            <p v-if="item.sizes?.length == 1">
+              {{ item.sizes[0].price }},-
+            </p>
+            <ul v-else class="" >
+              <li class="font-semibold" v-for="size in item.sizes">
+                {{ size.size }}: {{ size.price }},-
+              </li>
+            </ul>
+          </div>
         </li>
       </ul>
 
